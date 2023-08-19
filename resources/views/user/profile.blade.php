e81f3115fe622755d2d13c0aeb5da15e0fd305d6
João Silva <109978656+JOao7640@users.noreply.github.com>
2023-08-19 22:49:35 +0100

    Added the user description

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -2,22 +2,31 @@
 
 @section('content')
 
-        <div class="p-8 flex">
+    <div class="p-8">
+        <div class="p-8 flex bg-gray-100 rounded">
             <div class="sm:w-28 sm:h-28 md:w-32 md:h-32 lg:w-32 md:w-32 w-24 h-24 rounded-full overflow-hidden">
                 <img src="/images/profilePicture.jpg"
                      alt="profile picture"
                      class="object-cover border-b-2 shadow-2xl"
                 >
             </div>
-            <div class="ml-6 flex">
-                <div class="flex flex-col">
+            <div class="ml-6">
+                <div class="flex flex-col items-start">
                     <h1 class="text-xl font-semibold">{{$user->name}}</h1>
-                    <p class="text-xl">{{$user->username}}</p>
+                    <p class="text-md text-gray-500 mt-1">
+                        <spam>{{$user->username}}</spam>
+                    </p>
+                    @if($user->description !== null)
+                        <p class="mt-3">{{$user->description}}</p>
+
+                    @else
+                        <p class="mt-3">This user haven't set their description yet :/</p>
+                    @endif
+
                 </div>
             </div>
-
         </div>
-
-        <x-post-list :posts="$posts"/>
+    </div>
+    <x-post-list :posts="$posts"/>
 
 @endsection
